7d027f9af33afe0d03a36c63c813afe3624bedd1
Mohammad Ali <123582111+MohammadAliiter@users.noreply.github.com>
2023-07-04 13:54:11 +0530

    Add files via upload

---
+++ CalculatetheASCIIvalueofChar.java
@@ -0,0 +1,25 @@
+public class CalculatetheASCIIvalueofChar {
+    public static void main(String[] args) {
+      char char1 = 'z';
+        int i = char1;
+        System.out.println(i + " " + " this is the ascii value of the character");
+    
+    
+    
+    
+    
+        /*    
+    // this is another program how to find the length of the string. in given below you can see there are 
+     two methode are implement one only siple write str.length;
+        String a = "bhaijaan";
+        int b = a.length();
+        int len = 0;
+        for(char c1 : a.toCharArray()){
+          len++;
+        }
+          System.out.println(len);
+          System.out.println(b);
+          */
+
+    }
+}
