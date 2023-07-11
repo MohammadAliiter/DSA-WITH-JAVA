05a747d74f0f7de3f498b0f1d9d54fea526c22e9
Mohammad Ali <123582111+MohammadAliiter@users.noreply.github.com>
2023-07-11 13:24:22 +0530

    Add files via upload

---
+++ reversebyitration.java
@@ -0,0 +1,18 @@
+public class reversebyitration {
+    public static void main(String[] args) {
+        String inputString = "Hello, World!";
+        String reversedString = reverseString(inputString);
+        System.out.println("Reversed String: " + reversedString);
+    }
+
+    public static String reverseString(String str) {
+        // Base case: if the string is empty or has only one character
+        if (str.isEmpty() || str.length() == 1) {
+            return str;
+        }
+        
+        // Recursive case: reverse the substring from index 1 to end and append the first character at the end
+        return reverseString(str.substring(1)) + str.charAt(0);
+    }
+}
+
